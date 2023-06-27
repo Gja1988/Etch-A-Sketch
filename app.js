ef4bfce980feb00b4472fbf50ed1246417170715
Guillermo <garauz1988@gmail.com>
2023-06-26 21:34:46 -0400

    Start project & create functions, html, & styling

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
-
 const container = document.querySelector('.container');
+const resetButton = document.querySelector('.reset');
+const blackInkButton = document.querySelector('.black');
+const rainbowInkButton = document.querySelector('.rainbow');
+let currentListener = null;
 
-// for (let i = 0; i < 16; i++) {
-//     for (let j = 0; j < 16; j++) {
-//         const div = document.createElement('div');
-//         container.append(div);
-//     }
-// }
 const makeRows = () => {
     for (let i = 0; i < 16; i++) {
         const row = document.createElement('div');
-        row.classList.add('row');
-        row.textContent = 'test'
+        row.classList.add('box');
         container.append(row);
     }
 }
@@ -21,12 +17,67 @@ const makeColumns = () => {
         for (let j = 0; j < 16; j++) {
 
             const column = document.createElement('div');
-            column.classList.add('column');
-            column.textContent = 'okay'
+            column.classList.add('box');
             container.append(column);
         }
     }
 }
 
+resetButton.addEventListener('click', () => {
+    container.innerHTML = '';
+    makeRows();
+    makeColumns();
+    addBlackListener();
+});
+
+const blackBoxes = (e) => {
+    e.target.style.backgroundColor = 'black';
+}
+
+const removeListeners = () => {
+    const boxes = document.querySelectorAll('.box');
+    boxes.forEach((box) => {
+        box.removeEventListener('mouseenter', blackBoxes);
+        box.removeEventListener('mouseenter', coloredBoxes);
+    });
+};
+
+const coloredBoxes = (e) => {
+
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+
+    const color = `rgb(${r}, ${g}, ${b}`;
+    e.target.style.backgroundColor = color;
+};
+
+const addBlackListener = () => {
+
+    removeListeners();
+    const boxes = document.querySelectorAll('.box');
+    boxes.forEach(function (box) {
+        box.addEventListener('mouseenter', blackBoxes)
+    });
+}
+
+const addColoredListener = () => {
+
+    removeListeners();
+    const boxes = document.querySelectorAll('.box');
+    boxes.forEach((box) => {
+        box.addEventListener('mouseenter', coloredBoxes);
+    });
+};
+
+rainbowInkButton.addEventListener('click', () => {
+    addColoredListener();
+});
+
+blackInkButton.addEventListener('click', () => {
+    addBlackListener();
+})
+
 makeRows();
 makeColumns();
+addBlackListener();
