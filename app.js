cf800cff09fef313c165ec88be864cfd64013f44
Guillermo <garauz1988@gmail.com>
2023-06-28 17:09:29 -0400

    Create functions, buttons, and event listeners for project.

--- app.js
+++ app.js
@@ -2,16 +2,51 @@ const container = document.querySelector('.container');
 const resetButton = document.querySelector('.reset');
 const blackInkButton = document.querySelector('.black');
 const rainbowInkButton = document.querySelector('.rainbow');
-let currentListener = null;
+const gridSize = document.querySelector('.grid');
+let gridNum;
 
-const makeRows = () => {
-    for (let i = 0; i < 16; i++) {
-        const row = document.createElement('div');
-        row.classList.add('box');
-        container.append(row);
+
+// Function to store a user inputted number < 100 to change grid. 
+const storeGrid = () => {
+
+    while (true) {
+        gridNum = prompt('Please type a number less than 100 to change grid size');
+
+        if (isNaN(gridNum) || gridNum > 100) {
+            alert('Please enter a valid number less than 100');
+        } else {
+
+            return gridNum;
+        }
     }
 }
 
+// Event Listener to change grid size after pushing button 
+gridSize.addEventListener('click', () => {
+    container.innerHTML = '';
+    const storedGrid = storeGrid();
+
+    for (let i = 0; i < storedGrid; i++) {
+        const row = document.createElement('div');
+        row.classList.add('box');
+        container.append(row);
+        container.style.gridTemplateRows = `repeat(${storedGrid}, 1fr)`;
+    }
+
+    for (let i = 0; i < storedGrid; i++) {
+        for (let j = 0; j < storedGrid; j++) {
+
+            const column = document.createElement('div');
+            column.classList.add('box');
+            container.append(column);
+            container.style.gridTemplateColumns = `repeat(${storedGrid}, 1fr)`;
+        }
+    }
+
+    addBlackListener();
+})
+
+// Function to make a 16x16 grid.
 const makeColumns = () => {
     for (let i = 0; i < 16; i++) {
         for (let j = 0; j < 16; j++) {
@@ -23,25 +58,23 @@ const makeColumns = () => {
     }
 }
 
+// Click event listener to reset the grid and start again at 16x16 with black ink. 
 resetButton.addEventListener('click', () => {
+    removeListeners();
     container.innerHTML = '';
-    makeRows();
+    container.style.gridTemplateColumns = `repeat(16, 1fr)`;
+    container.style.gridTemplateRows = `repeat(16, 1fr)`;
+    // makeRows();
     makeColumns();
     addBlackListener();
 });
 
+// Function to fill each box with a black background.
 const blackBoxes = (e) => {
     e.target.style.backgroundColor = 'black';
 }
 
-const removeListeners = () => {
-    const boxes = document.querySelectorAll('.box');
-    boxes.forEach((box) => {
-        box.removeEventListener('mouseenter', blackBoxes);
-        box.removeEventListener('mouseenter', coloredBoxes);
-    });
-};
-
+// Function to fill each box with a random background color. 
 const coloredBoxes = (e) => {
 
     const r = Math.floor(Math.random() * 256);
@@ -52,6 +85,16 @@ const coloredBoxes = (e) => {
     e.target.style.backgroundColor = color;
 };
 
+// Function to remove all event listeners within the container to reset grid properly.
+const removeListeners = () => {
+    const boxes = document.querySelectorAll('.box');
+    boxes.forEach((box) => {
+        box.removeEventListener('mouseenter', blackBoxes);
+        box.removeEventListener('mouseenter', coloredBoxes);
+    });
+};
+
+// Event listener to add the black ink for each box when mouse enters the box.
 const addBlackListener = () => {
 
     removeListeners();
@@ -61,6 +104,7 @@ const addBlackListener = () => {
     });
 }
 
+// Event listener to add the rainbow ink for each box when mouse enters the box.
 const addColoredListener = () => {
 
     removeListeners();
@@ -70,14 +114,15 @@ const addColoredListener = () => {
     });
 };
 
+// Click listener to change the color of rainbow when used for the grid.
 rainbowInkButton.addEventListener('click', () => {
     addColoredListener();
 });
 
+// Click listener to change the color of black when used for the grid.
 blackInkButton.addEventListener('click', () => {
     addBlackListener();
 })
-
-makeRows();
+// Functions called to start the project. 
 makeColumns();
 addBlackListener();
